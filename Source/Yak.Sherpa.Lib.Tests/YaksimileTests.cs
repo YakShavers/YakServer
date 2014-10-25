73b5da3cd0c93806bdfee4577bdd497bb18b544f
Steve Cooper <steve@stevecooper.org>
2014-10-25 16:54:25 +0100

    Started on the Yaksimile yak emulator

---
+++ Source/Yak.Sherpa.Lib.Tests/YaksimileTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Yak.Sherpa.Lib.Tests
+{
+    [TestClass]
+    public class YaksimileTests
+    {
+        [TestMethod]
+        public void Yaksimile_OpensAndCloses()
+        {
+            var yaksimile = new Yak.Yaksimile.Yaksimile();
+            yaksimile.Initialize();
+            Thread.Sleep(500);
+            yaksimile.TearDown();
+        }
+    }
+}
--- Source/Yak.Yaksimile/Yaksimile.cs
+++ Source/Yak.Yaksimile/Yaksimile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using Yak.Sherpa.Lib;
@@ -8,6 +9,8 @@ namespace Yak.Yaksimile
 {
     public class Yaksimile: IYakGPIO
     {
+        private YakscimileForm form;
+
         public void SetPin(Pin pin, bool high)
         {
             
@@ -17,11 +20,37 @@ namespace Yak.Yaksimile
         public void Initialize()
         {
             // show the yak window!
+            this.form = new YakscimileForm();
+            this.form.Show();
         }
 
         public void TearDown()
         {
             // hide the yak window
+            this.form.Hide();
+            this.form = null;
+        }
+    }
+
+    public class YakscimileForm : System.Windows.Forms.Form
+    {
+        public YakscimileForm()
+        {
+            var scale = 0.25;
+
+            var backgroundImage = Resources.FormResources.YaksimileBackground;
+
+            var scaleWidth = (int)(scale * backgroundImage.Width);
+            var scaleHeight = (int)(scale * backgroundImage.Height);
+
+            var scaledImage = new Bitmap(scaleWidth, scaleHeight); 
+            var graph = Graphics.FromImage(scaledImage);
+
+            graph.DrawImage(backgroundImage, 0,0,scaleWidth, scaleHeight);
+
+            this.BackgroundImage = scaledImage;
+            this.Width = scaleWidth;
+            this.Height = scaleHeight;
         }
     }
 }
